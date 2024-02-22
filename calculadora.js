4b44a837b7ad643fdd244dcebfd186794dedde62
nadyeskaquint <142436834+nadyeska@users.noreply.github.com>
2024-02-22 12:05:48 -0300

    Delete calculadora.js

--- calculadora.js
+++
@@ -1,7 +0,0 @@
-const calcular = () => {
-    let numero1 = document.getElementById("numero1");
-    let numero2 = document.getElementById("numero2");
-    let resultao = document.getElementById("resultado");
-
-    resultado.value = parseInt(numero1.value) + parseInt(numero2.value);
-}
